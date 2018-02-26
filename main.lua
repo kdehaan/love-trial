d39c3e4c35d691cc2f9526addbf837c0b38499fa
Kevin de Haan <kbehaan@gmail.com>
2018-02-25 21:13:56 -0700

    local directory working

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
+function love.load()
+    image = love.graphics.newImage('/res/image.jpg')
+end
+
+function love.update(dt)
+
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.draw(image, love.math.random(0, 800), 0)
 end
